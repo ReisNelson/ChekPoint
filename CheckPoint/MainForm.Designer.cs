5f4ccfbbba11f7f06d7966440fd3117f0b27c076
Nelson Dos Reis Gomes Souza <nelsondosreisgomessouza@gmail.com>
2025-09-27 16:19:53 -0300

    Update Adiciona código base do Projeto

---
+++ CheckPoint/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CheckPoint
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEntrada = new System.Windows.Forms.Button();
+            this.btnSaida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEntrada
+            // 
+            this.btnEntrada.Location = new System.Drawing.Point(329, 154);
+            this.btnEntrada.Name = "btnEntrada";
+            this.btnEntrada.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrada.TabIndex = 0;
+            this.btnEntrada.Text = "Entrada";
+            this.btnEntrada.UseVisualStyleBackColor = true;
+            this.btnEntrada.Click += new System.EventHandler(this.Entrada_Click);
+            // 
+            // btnSaida
+            // 
+            this.btnSaida.Location = new System.Drawing.Point(329, 204);
+            this.btnSaida.Name = "btnSaida";
+            this.btnSaida.Size = new System.Drawing.Size(75, 23);
+            this.btnSaida.TabIndex = 2;
+            this.btnSaida.Text = "Saída";
+            this.btnSaida.UseVisualStyleBackColor = true;
+            this.btnSaida.Click += new System.EventHandler(this.Saida_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaida);
+            this.Controls.Add(this.btnEntrada);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "CheckPoint";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEntrada;
+        private System.Windows.Forms.Button btnSaida;
+    }
+}
+
---
+++ CheckPoint/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckPoint
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Entrada_Click(object sender, EventArgs e) { RegistrarPonto("Entrada");}
+
+
+        private void Saida_Click(object sender, EventArgs e) { RegistrarPonto("Saída");}
+        
+
+        private void RegistrarPonto(string tipo)
+        {
+         try
+            {
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var fileName = $"{tipo}_{DateTime.Now:MM}.txt";
+                var pathCombined = System.IO.Path.Combine(desktop, fileName);
+                var linha = $"Dia - {DateTime.Now:dd - HH:mm}{Environment.NewLine}";
+                
+                File.AppendAllText(pathCombined, linha);
+                MessageBox.Show($"Ponto de {tipo} Registrado com Sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao registrar ponto de {tipo}: {ex.Message}");
+            }
+        }
+    }
+}
